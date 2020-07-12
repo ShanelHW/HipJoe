35e710fa4c95cd4dff859e9ad5c7eb48b0dd81c4
Shanel Davis <nshanelh@gmail.com>
2020-07-11 23:31:45 -0400

    color corrections

--- index.js
+++ index.js
@@ -4,21 +4,22 @@
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none"; 
+        
     }
 
-    // Remove the background color of all tablinks/buttons
+    // Remove the background color of all tablinks
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].style.backgroundColor = "";
     }
-
+    
     // Show the specific tab content
     document.getElementById(tabName).style.display = "block";
+   
 
     // Add the specific color to the button used to open the tab content
     elmnt.style.backgroundColor = color;
 }
-
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
